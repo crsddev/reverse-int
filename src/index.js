012b9fb58e504b41ef7f69ef35d86ee4c5fd5633
crsddev <againnft@gmail.com>
2024-09-14 14:12:06 +0300

    rsReverseInt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let temp = n + "";
+    let rsl = "";
+    for (let i = temp.length - 1; i >= 0; i--) {
+        if (temp[i] !== '-') {
+            rsl += temp[i];
+        }
+    }
+    return rsl;
 }
